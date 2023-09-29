96c6637081caa7e7ec16fc017b8b9723c63db06d
Scott Lucas <scott@slucas.com>
2023-09-29 16:00:58 -0700

    Initial commit

--- Package.swift
+++ Package.swift
@@ -5,17 +5,25 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftyPingAsync",
+    platforms: [
+      .iOS(.v13), .tvOS(.v13), .macOS(.v11)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftyPingAsync",
             targets: ["SwiftyPingAsync"]),
     ],
+    dependencies: [
+      // Dependencies declare other packages that this package depends on.
+      .package(url: "https://github.com/samiyr/SwiftyPing", from: "1.2.1"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwiftyPingAsync"),
+            name: "SwiftyPingAsync",
+            dependencies: ["SwiftyPing"]),
         .testTarget(
             name: "SwiftyPingAsyncTests",
             dependencies: ["SwiftyPingAsync"]),
--- Sources/SwiftyPingAsync/SwiftyPingAsync.swift
+++ Sources/SwiftyPingAsync/SwiftyPingAsync.swift
@@ -1,2 +1,80 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+   // The Swift Programming Language
+   // https://docs.swift.org/swift-book
+
+import SwiftyPing
+import Foundation
+
+struct AsyncPing {
+   
+   mutating func ping () -> AsyncThrowingStream<PingResponse, Error> {
+      let stream = AsyncThrowingStream<PingResponse, Error> { cont in
+         pinger.finished = { _ in
+            cont.finish()
+         }
+         pinger.observer = { result in
+            cont.yield(result)
+         }
+         do {
+            try pinger.startPinging()
+         } catch {
+            cont.finish(throwing: error)
+         }
+      }
+      return stream
+   }
+   
+   mutating func pingOnce () -> AsyncThrowingStream<PingResponse, Error> {
+      let stream = AsyncThrowingStream<PingResponse, Error> { cont in
+         pinger.finished = { _ in
+            cont.finish()
+         }
+         pinger.observer = { result in
+            cont.yield(result)
+         }
+         do {
+            pinger.targetCount = 1
+            try pinger.startPinging()
+         } catch {
+            cont.finish(throwing: error)
+         }
+      }
+      return stream
+   }
+   
+   func pingResult () async throws -> PingResult {
+      try await withCheckedThrowingContinuation { cont in
+         pinger.finished = { result in
+            cont.resume(returning: result)
+         }
+         do {
+            try pinger.startPinging()
+         } catch {
+            cont.resume(throwing: error)
+         }
+      }
+   }
+   
+   mutating func stopPinging() {
+      pinger.stopPinging()
+   }
+   
+   private var pinger: SwiftyPing
+   
+   init(ipv4Address: String, config configuration: PingConfiguration, count: Int? = nil, queue: DispatchQueue) throws {
+      let pinger = try SwiftyPing(ipv4Address: ipv4Address, config: configuration, queue: queue)
+      pinger.targetCount = count
+      self.pinger = pinger
+   }
+   init(destination: SwiftyPing.Destination, configuration: PingConfiguration, count: Int? = nil, queue: DispatchQueue) throws {
+      let pinger = try SwiftyPing(destination: destination, configuration: configuration, queue: queue)
+      pinger.targetCount = count
+      self.pinger = pinger
+   }
+   init(host: String, configuration: PingConfiguration, count: Int? = nil, queue: DispatchQueue) throws {
+      let pinger = try SwiftyPing(host: host, configuration: configuration, queue: queue)
+      pinger.targetCount = count
+      self.pinger = pinger
+   }
+}
+
+
