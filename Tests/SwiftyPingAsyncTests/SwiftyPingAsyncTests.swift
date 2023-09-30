b33509e3e3db9624ae7ee03e60250d91a7ba5fce
Scott Lucas <scott@slucas.com>
2023-09-30 12:03:11 -0700

    Added unit test.

--- Tests/SwiftyPingAsyncTests/SwiftyPingAsyncTests.swift
+++ Tests/SwiftyPingAsyncTests/SwiftyPingAsyncTests.swift
@@ -1,12 +1,106 @@
 import XCTest
 @testable import SwiftyPingAsync
 
+//Unit tests utilize an external source for pings, which can and will cause false test failures.
 final class SwiftyPingAsyncTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
-    }
+   func testPingIPv4() async throws {
+      var byteCounts = [Int?]()
+      var errors = [PingError?]()
+      var addresses = [String?]()
+      var headers = [IPHeader?]()
+      var start = Date()
+      do {
+         var pinger = try AsyncPing(ipv4Address: "8.8.8.8", config: .init(interval: 0.5, with: 1.0), queue: .global())
+         pinger.targetCount = 10
+         for try await ping in pinger.ping() {
+            byteCounts.append(ping.byteCount)
+            errors.append(ping.error)
+            addresses.append(ping.ipAddress)
+            headers.append(ping.ipHeader)
+         }
+      } catch {
+         assertionFailure()
+      }
+      XCTAssertLessThan(Date().timeIntervalSince(start), 5.0)
+      XCTAssertEqual(byteCounts.compactMap({ $0 }).count, 10)
+      XCTAssertEqual(errors.compactMap({ $0 }).count, 0)
+      XCTAssertEqual(addresses.compactMap({ $0 }).count, 10)
+      XCTAssertEqual(headers.compactMap({ $0 }).count, 10)
+   }
+   func testPingHostName() async throws {
+      var count = 0
+      var errorCount = 0
+      do {
+         var pinger = try AsyncPing(host: "google.com", configuration: .init(interval: 0.5, with: 1.0), queue: .global())
+         pinger.targetCount = 10
+         for try await ping in pinger.ping() {
+            if ping.error != nil { errorCount += 1 }
+            count += 1
+         }
+      } catch {
+         assertionFailure()
+      }
+      XCTAssertEqual(count, 10)
+      XCTAssertEqual(errorCount, 0)
+   }
+   func testPingOnce () async throws {
+      var count = 0
+      var errorCount = 0
+      do {
+         var pinger = try AsyncPing(host: "google.com", configuration: .init(interval: 0.5, with: 1.0), queue: .global())
+         for try await ping in pinger.pingOnce() {
+            if ping.error != nil { errorCount += 1 }
+            count += 1
+         }
+      } catch {
+         assertionFailure()
+      }
+      XCTAssertEqual(count, 1)
+      XCTAssertEqual(errorCount, 0)
+   }
+   func testPingResult () async throws {
+      var result: PingResult?
+      do {
+         var pinger = try AsyncPing(host: "google.com", configuration: .init(interval: 0.5, with: 1.0), queue: .global())
+         result = try await pinger.pingResult()
+      } catch {
+         assertionFailure()
+      }
+      XCTAssertNotNil(result)
+      XCTAssertNotNil(result?.packetLoss)
+      XCTAssertNotNil(result?.packetsReceived)
+      XCTAssertNotNil(result?.packetsTransmitted)
+      XCTAssertNotNil(result?.roundtrip)
+      XCTAssertNotNil(result?.roundtrip?.average)
+      XCTAssertNotNil(result?.roundtrip?.maximum)
+      XCTAssertNotNil(result?.roundtrip?.minimum)
+      XCTAssertNotNil(result?.roundtrip?.standardDeviation)
+      XCTAssertEqual(result!.packetLoss, 0)
+      XCTAssertEqual(result!.packetsReceived, 10)
+      XCTAssertEqual(result!.packetsTransmitted, 10)
+      XCTAssertEqual(result!.responses.count, 10)
+   }
+   func testPingResultWithPacketCount () async throws {
+      var result: PingResult?
+      do {
+         var pinger = try AsyncPing(host: "google.com", configuration: .init(interval: 0.5, with: 1.0), queue: .global())
+         pinger.targetCount = 5
+         result = try await pinger.pingResult()
+      } catch {
+         assertionFailure()
+      }
+      XCTAssertNotNil(result)
+      XCTAssertNotNil(result?.packetLoss)
+      XCTAssertNotNil(result?.packetsReceived)
+      XCTAssertNotNil(result?.packetsTransmitted)
+      XCTAssertNotNil(result?.roundtrip)
+      XCTAssertNotNil(result?.roundtrip?.average)
+      XCTAssertNotNil(result?.roundtrip?.maximum)
+      XCTAssertNotNil(result?.roundtrip?.minimum)
+      XCTAssertNotNil(result?.roundtrip?.standardDeviation)
+      XCTAssertEqual(result!.packetLoss, 0)
+      XCTAssertEqual(result!.packetsReceived, 5)
+      XCTAssertEqual(result!.packetsTransmitted, 5)
+      XCTAssertEqual(result!.responses.count, 5)
+   }
 }
